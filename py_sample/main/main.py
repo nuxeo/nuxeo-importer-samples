11dddd909bd48f235095498d4063499ec285e99f
Andrei Nechaev <anechaev@nuxeo.com>
2016-11-15 14:16:27 -0500

    NXP-21023 sending messages one by one

--- py_sample/main/main.py
+++ py_sample/main/main.py
@@ -4,6 +4,7 @@ import sys
 # from avro.datafile import DataFileReader
 # from avro.io import DatumReader
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
 
 from py_sample.main.traverse import traverse
 
@@ -13,20 +14,12 @@ producer = KafkaProducer(
 l_created = []
 
 
-def send(buf, level):
-    producer.send('level_1', value=buf.getvalue(), key=bytes('msg', 'utf-8'))
-    # reader = DataFileReader(buf, DatumReader())
-    # for msg in reader:
-    #     print('Reading: ' + str(msg) + ' on level ' + str(level))
-    producer.flush()
-    # reader.close()
-
-
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         raise Exception('Please, provide the path')
     p = sys.argv[1]
     if not os.path.exists(p):
         raise Exception('Path ' + p + ' does not exist')
-    traverse(p, send, producer)
+    traverse(p, producer)
+    producer.flush()
     producer.close()
--- py_sample/main/traverse.py
+++ py_sample/main/traverse.py
@@ -4,30 +4,43 @@ import os
 import avro
 from avro.datafile import DataFileWriter
 from avro.io import DatumWriter
+from kafka.errors import KafkaError
 
 from py_sample.item import Item
 
+sch = avro.schema.Parse(open('../../message.avsc').read())
 
-def traverse(path: str, func, producer=None):
+
+def traverse(path: str, producer):
     sp = path.split(sep='/')
     lp = len(sp)
-    sch = avro.schema.Parse(open('../../message.avsc').read())
     for root, dirs, files in os.walk(path, topdown=True):
         sr = root.split('/')
+        level = len(sr) - lp
+        prepare(producer, dirs, root, level)
+        prepare(producer, files, root, level)
+
+
+def prepare(producer, arr, root, level):
+    for it in arr:
         buf = io.BytesIO()
         writer = DataFileWriter(buf, DatumWriter(), sch)
-
-        level = len(sr) - lp
-        prepare(dirs, writer, root)
-        prepare(files, writer, root)
-        writer.flush()
-        if producer is None:
-            func(buf, level)
-        else:
-            func(buf, level)
-
-
-def prepare(arr, writer, root):
-    for it in arr:
         item = Item(root, it, False)
         writer.append(item.get_dict())
+        writer.flush()
+        send(buf, level, producer)
+
+
+def send(buf, level, producer):
+    lv = 'level_' + str(level)
+    future = producer.send(
+        topic=lv,
+        value=buf.getvalue(),
+        key=bytes('msg', 'utf-8')
+    )
+    # await async send
+    try:
+        metadata = future.get(timeout=1000)
+        print(metadata)
+    except KafkaError:
+        print("Couldn't send message to level " + str(lv))
